b6e2ea900bf56cb4fcc174e508c117b67ca3affc
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-22 11:28:08 +0300

    updating

---
+++ gemwallet/src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYfVoKK-DxdON0IonyoI0HSMxMp7JTyDE",
+  authDomain: "gemwallet-f143d.firebaseapp.com",
+  projectId: "gemwallet-f143d",
+  storageBucket: "gemwallet-f143d.firebasestorage.app",
+  messagingSenderId: "445046659265",
+  appId: "1:445046659265:web:6b48c58db78bae838adc40",
+  measurementId: "G-Q00FCSZL0H"
+};
+
+export default firebaseConfig;
